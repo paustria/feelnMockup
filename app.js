1e4a0bf994eaa0ac7dafb4e380d3f1f4f809f704
Phily Austria <austriaphily@gmail.com>
2016-04-21 17:37:17 -0700

    Set up webpack.

---
+++ app.js
@@ -0,0 +1,4 @@
+require("jquery");
+require("bootstrap-sass");
+require("./scss/style.scss");
+document.write(require("./js/content.js"));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require("webpack");
+var BowerWebpackPlugin = require('bower-webpack-plugin');
+var path = require("path");
+
+module.exports = {
+    entry: "./app.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/build/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.scss$/, loaders: ["style", "css", "sass"]},
+            { test: /\.(woff|svg|ttf|eot)([\?]?.*)$/, loader: "file-loader?name=[name].[ext]" }
+        ]
+    },
+    plugins: [
+        new BowerWebpackPlugin({
+        }),
+        new webpack.ProvidePlugin({
+            $:      "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    resolve: {
+        modulesDirectories: ["web_modules", "node_modules", "bower_components"]
+    },
+    devServer: {
+        port: 8008
+    }
+};
