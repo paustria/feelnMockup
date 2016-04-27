1eb9304b2a1d8e8c9e29604884f1c1e65cd94a83
Phily Austria <austriaphily@gmail.com>
2016-04-26 17:11:47 -0700

    Made carousel responsive.

--- js/content.js
+++ js/content.js
@@ -9,7 +9,32 @@ module.exports = function () {
             infinite: true,
             lazyLoad: 'ondemand',
             slidesToShow: 3,
-            slidesToScroll: 1
+            slidesToScroll: 3,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
 
         $('[data-toggle="popover"]').popover();
